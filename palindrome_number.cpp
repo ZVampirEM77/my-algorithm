8d50423ef505d4f7e3b705f56c5357157009a4f2
Enming.Zhang <zvampirem77@gmail.com>
2019-05-04 12:18:33 -0400

    9. Palindrome Number
    solve the problem without converting the integer
    to a string
    
    Signed-off-by: Enming.Zhang <zvampirem77@gmail.com>

--- palindrome_number.cpp
+++ palindrome_number.cpp
@@ -15,3 +15,33 @@ public:
         return true;
     }
 };
+
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {  // -121
+            return false;
+        } else if (x < 10) {  // 1
+            return true;
+        } else if (x % 10 == 0) {  // 21120
+            return false;
+        }
+
+        int cmp_x = 0;
+        while (x > cmp_x) {
+            cmp_x = cmp_x * 10 + (x % 10);
+            x = x / 10;
+        }
+
+        if (cmp_x > x) {
+            if (x != 0 && (cmp_x / 10) == x) {  // 10
+                return true;
+            }
+        } else if (cmp_x == x) {
+            return true;
+        }
+
+        return false;
+    }
+};
