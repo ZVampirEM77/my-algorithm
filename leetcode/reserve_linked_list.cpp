a6c951704497416c35b6bdf67ee8a36631de7337
张恩铭 <damon.zhang@zhangenmingdeMacBook-Pro.local>
2020-06-26 18:48:22 +0800

    add reserve linked list
    Signed-off-by: Enming.Zhang <zvampirem77@gmail.com>

---
+++ leetcode/reserve_linked_list.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) {
+            return nullptr;
+        } else if (head->next == nullptr) {
+            return head;
+        }
+        
+        ListNode * cur_node = head;
+        ListNode * pre_node = nullptr;
+        ListNode * next_node = nullptr;
+
+        while(cur_node != nullptr) {
+            next_node = cur_node->next;
+            cur_node->next = pre_node;
+            pre_node = cur_node;
+            cur_node = next_node;
+        }
+        
+        return pre_node;
+    }
+};
