a1e7a1eb182059984c0db2cece848a61c5012e52
Enming Zhang <zvampirem77@gmail.com>
2020-06-27 16:49:34 +0800

    add generate cycled linked list
    Signed-off-by: Enming Zhang <zvampirem77@gmail.com>

--- leetcode/linked_list_op.h
+++ leetcode/linked_list_op.h
@@ -18,3 +18,37 @@ T* CreateLinkedList(int n) {
 
     return head;
 }
+
+template <class T>
+T* CreateCycledLinkedList(int n) {
+    if (!n) {
+        return nullptr;
+    }
+
+    srand((unsigned int)time(NULL));
+    T * head = new T(rand());
+
+    if (n == 1) {
+        return head;
+    }
+
+    T * cur_node = head;
+    int rand_index = rand() % (n - 1);
+    T * rand_node = nullptr;
+
+    for (int i = 1; i < n; i++) {
+        cur_node->next = new T(rand());
+        cur_node = cur_node->next;
+        if (i == rand_index) {
+            rand_node = cur_node;
+        }
+    }
+
+    if (rand_node == nullptr) {
+        cur_node->next = head;
+    } else {
+        cur_node->next = rand_node;
+    }
+
+    return head;
+}
