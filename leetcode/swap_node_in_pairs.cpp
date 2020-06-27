a8af1fb5406c14e90dfa85d9a3be4de663473e35
Enming Zhang <zvampirem77@gmail.com>
2020-06-27 15:38:34 +0800

    add swap nodes in pairs
    Signed-off-by: Enming Zhang <zvampirem77@gmail.com>

---
+++ leetcode/swap_node_in_pairs.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "linked_list_op.h"
+
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode * cur_node = head;
+        ListNode ** pointer_to_node_pointer = &head;
+
+        while(cur_node != nullptr && cur_node->next != nullptr) {
+            ListNode * adjacent_node = cur_node->next;
+            cur_node->next = adjacent_node->next;
+            adjacent_node->next = cur_node;
+            *pointer_to_node_pointer = adjacent_node;
+
+            pointer_to_node_pointer = &(cur_node->next);
+            cur_node = cur_node->next;
+        }
+
+        return head;
+    }
+};
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+
+        ListNode * cur_node = head;
+        ListNode * pre_node = new ListNode();
+        pre_node->next = head;
+        ListNode * new_head = cur_node->next;
+
+        while(cur_node != nullptr && cur_node->next != nullptr) {
+            ListNode * adjacent_node = cur_node->next;
+            cur_node->next = adjacent_node->next;
+            adjacent_node->next = cur_node;
+            pre_node->next = adjacent_node;
+
+            pre_node = cur_node;
+            cur_node = cur_node->next;
+        }
+
+        return new_head;
+    }
+};
