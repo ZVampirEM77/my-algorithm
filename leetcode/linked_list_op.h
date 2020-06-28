92d50c8317e1e531cfcbb871fea654765aaf40ff
Enming Zhang <zvampirem77@gmail.com>
2020-06-28 21:02:29 +0800

    reload function CreateCycledLinkedList
    Signed-off-by: Enming Zhang <zvampirem77@gmail.com>

--- leetcode/linked_list_op.h
+++ leetcode/linked_list_op.h
@@ -52,3 +52,38 @@ T* CreateCycledLinkedList(int n) {
 
     return head;
 }
+
+template <class T>
+T* CreateCycledLinkedList(int n, int index) {
+    if (!n) {
+        return nullptr;
+    }
+
+    srand((unsigned int)time(NULL));
+    T * head = new T(rand());
+    std::cout << "c " << head->val << " index " << index << std::endl;
+
+    if (n == 1) {
+        return head;
+    }
+
+    T * cur_node = head;
+    T * cycle_node = nullptr;
+
+    for (int i = 1; i < n; i++) {
+        cur_node->next = new T(rand());
+        cur_node = cur_node->next;
+        std::cout << "c " << cur_node->val << std::endl;
+        if (i == index) {
+            cycle_node = cur_node;
+        }
+    }
+
+    if (cycle_node == nullptr) {
+        cur_node->next = head;
+    } else {
+        cur_node->next = cycle_node;
+    }
+
+    return head;
+}
